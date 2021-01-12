19e4176fc592e10b5b9173c5ecefee1b67228299
Avery Burne <burneav@gmail.com>
2021-01-11 20:20:17 -0500

    Added stop button for rain sound

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 
+let audio = new Audio('rain-01.mp3')
+
 const makeSound = function () {
+    let counter = 1
     console.log('hello')
-    let audio = new Audio('rain-01.mp3')
     audio.play()
     console.log(audio)
+}
+
+const stopSound = function () {
+    audio.pause()
 }
