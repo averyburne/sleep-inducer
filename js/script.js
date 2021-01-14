b40f0d05ca861c104f7cee6506e1dcfd3cb0ecc1
Avery Burne <burneav@gmail.com>
2021-01-13 20:21:47 -0500

    Consolidated starting and stopping sound into one button. Deleted other buttons references in html and javascript

--- js/script.js
+++ js/script.js
@@ -1,36 +1,31 @@
 
 let audio = new Audio('rain-01.mp3')
 let audio2 = new Audio('food-court-1.mp3')
+let rainBool = true
+let chatterBool = true
 
-$('#rain-stop-btn').hide()
-$('#chatter-stop-btn').hide()
-
-const makeSound = function (e) {
-    console.log(e)
-    let rain = document.getElementById('rain-btn')
-    audio.play()
-    $('#rain-btn').hide()
-    $('#rain-stop-btn').show()
-}
-
-const stopSound = function () {
-    audio.pause()
-    audio.currentTime = 0
-    $('#rain-btn').show()
-    $('#rain-stop-btn').hide()
+const makeSound = function () {
+    if (rainBool) {
+        audio.play()
+        rainBool = false
+        document.getElementById('rain-btn').innerHTML = "Stop Sound"
+    } else {
+        audio.pause()
+        audio.currentTime = 0
+        rainBool = true
+        document.getElementById('rain-btn').innerHTML = "Rain Sound"
+    }
 }
 
 const makeSound2 = function (e) {
-    console.log(e)
-    let rain = document.getElementById('chatter-btn')
-    audio2.play()
-    $('#chatter-btn').hide()
-    $('#chatter-stop-btn').show()
-}
-
-const stopSound2 = function () {
-    audio2.pause()
-    audio2.currentTime = 0
-    $('#chatter-btn').show()
-    $('#chatter-stop-btn').hide()
+    if (chatterBool) {
+        audio2.play()
+        chatterBool = false
+        document.getElementById('chatter-btn').innerHTML = "Stop Sound"
+    } else {
+        audio2.pause()
+        audio2.currentTime = 0
+        chatterBool = true
+        document.getElementById('chatter-btn').innerHTML = "Food Court"
+    }
 }
