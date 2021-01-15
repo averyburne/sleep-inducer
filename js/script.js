f69ae34fc9d8e46722f27de343353f6b29f19f60
Avery Burne <burneav@gmail.com>
2021-01-15 17:50:41 -0500

    looped audio and changed font color when audio plays

--- js/script.js
+++ js/script.js
@@ -10,13 +10,15 @@ const makeSound = function () {
         rainBool = false
         document.getElementById('rain-btn').innerHTML = "Stop Sound"
         document.body.style.backgroundImage = "url('https://media1.giphy.com/media/PbOaO2fedzQLm/giphy.gif')"
+        document.body.style.color = "white"
+        audio.loop = true
     } else {
         audio.pause()
         audio.currentTime = 0
         rainBool = true
         document.getElementById('rain-btn').innerHTML = "Rain Sound"
         document.body.style.backgroundImage = ''
-
+        document.body.style.color = "black"
     }
 }
 
@@ -26,11 +28,13 @@ const makeSound2 = function (e) {
         chatterBool = false
         document.getElementById('chatter-btn').innerHTML = "Stop Sound"
         document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/6a/93/ce/6a93ce18781a65188b97a2074c66eade.gif')"
+        document.body.style.color = "white"
     } else {
         audio2.pause()
         audio2.currentTime = 0
         chatterBool = true
         document.getElementById('chatter-btn').innerHTML = "Food Court"
         document.body.style.backgroundImage = ''
+        document.body.style.color = "black"
     }
 }
