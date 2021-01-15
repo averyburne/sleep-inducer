bebe07239b00190b542ed004d6c35f2960587842
Avery Burne <burneav@gmail.com>
2021-01-14 19:18:33 -0500

    Added background gifs for both sound effects, only appear when sound is playing

--- js/script.js
+++ js/script.js
@@ -9,11 +9,14 @@ const makeSound = function () {
         audio.play()
         rainBool = false
         document.getElementById('rain-btn').innerHTML = "Stop Sound"
+        document.body.style.backgroundImage = "url('https://media1.giphy.com/media/PbOaO2fedzQLm/giphy.gif')"
     } else {
         audio.pause()
         audio.currentTime = 0
         rainBool = true
         document.getElementById('rain-btn').innerHTML = "Rain Sound"
+        document.body.style.backgroundImage = ''
+
     }
 }
 
@@ -22,10 +25,12 @@ const makeSound2 = function (e) {
         audio2.play()
         chatterBool = false
         document.getElementById('chatter-btn').innerHTML = "Stop Sound"
+        document.body.style.backgroundImage = "url('https://i.pinimg.com/originals/6a/93/ce/6a93ce18781a65188b97a2074c66eade.gif')"
     } else {
         audio2.pause()
         audio2.currentTime = 0
         chatterBool = true
         document.getElementById('chatter-btn').innerHTML = "Food Court"
+        document.body.style.backgroundImage = ''
     }
 }
